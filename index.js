e230ad9089cecd53ebf2eb131bb0ce7d9fb6aa98
shillo <kimsemen0402@mail.ru>
2024-05-20 16:43:08 +0300

    Add HashMap (hash(), set(), show()), LinkedList and Node classes

--- index.js
+++ index.js
@@ -1,93 +1,202 @@
-class HashMap {
-    constructor() {
-        this.table = {}
+class Node {
+    constructor(value) {
+        this.value = value
+        this.nextNode = null
     }
-    hash(key) {
-        let hashCode = 0
+}
 
-        const primeNumber = 31
+class LinkedList {
+    constructor() {
+        this.head = new Node('head')
+    }
 
-        for (let i = 0; i < key.length; i++) {
-            hashCode = primeNumber * hashCode + key.charCodeAt(i) 
-            if (this.table.length) {
-                hashCode = hashCode % this.table.length
-            }
+    toString() {
+        let result = 'head -> '
+        let link = this.head.nextNode
+        while (link != null) {
+            result += `${link.value} -> `
+            link = link.nextNode
         }
 
+        return console.log(result + 'null')
+    }
+
+    append(value) {
+        let link = this.head
+        while (true) {
+            if (link.nextNode != null) {
+                link = link.nextNode
+            } else {
+                link.nextNode = new Node(value)
+                break
+            }
+        }
+    }
+
+    prepend(value) {
+        let newNode = new Node(value)
+        newNode.nextNode = this.head.nextNode
+        this.head.nextNode = newNode
+    }
+
+    size() {
+        let count = 0
+        let link = this.head.nextNode
+        while (link != null) {
+            count += 1
+            link = link.nextNode
+        }
+
+        return console.log(count)
+    }
+
+    showHead() {
+        return console.log(this.head.nextNode)
+    }
+
+    showTail() {
+        let link = this.head
+        while (true) {
+            if (link.nextNode != null) {
+                link = link.nextNode
+            } else {
+                return console.log(link)
+            }
+        }
+    }
+
+    at(index) {
+        let count = 0
+        let link = this.head.nextNode
+        while (true) {
+            if (count == index) {
+                return console.log(link)
+            } else if (link.nextNode == null) {
+                return console.log('Такого узла нет')
+            } else {
+                link = link.nextNode
+                count += 1
+            }
+        }
+    }
+
+    pop() {
+        let link = this.head
+        while (true) {
+            if (link.nextNode.nextNode == null) {
+                console.log(link)
+                link.nextNode = null
+                break
+            } else {
+                link = link.nextNode
+            }
+        }
+    }
+
+    contains(value) {
+        let link = this.head.nextNode
+        while (link != null) {
+            if (link.value == value) {
+                return console.log(true)
+            }
+            link = link.nextNode
+        }
+        return console.log(false)
+    }
+    
+    find(value) {
+        let count = 0
+        let link = this.head.nextNode
+        while (link != null) {
+            if (link.value == value) {
+                return console.log(count)
+            }
+            link = link.nextNode
+            count += 1
+        }
+        return console.log(null)
+    }
+
+    insertAt(value, index) {
+        let count = 0
+        let link = this.head
+        let a = this
+        while (true) {
+            if (count == index) {
+                let newNode = new Node(value)
+                newNode.nextNode = link.nextNode
+                link.nextNode = newNode
+                a.toString()
+                break
+            } else if (link.nextNode == null) {
+                return console.log('Такого индекса нет')
+            } else {
+                link = link.nextNode
+                count += 1
+            }
+        }
+    }
+
+    removeAt(index) {
+        let count = 0
+        let link = this.head
+        let a = this
+        while (true) {
+            if (count == index) {
+                link.nextNode = link.nextNode.nextNode
+                a.toString()
+                break
+            } else if (link.nextNode == null) {
+                return console.log('Такого индекса нет')
+            } else {
+                link = link.nextNode
+                count += 1
+            }
+        }
+    }
+}
+
+class HashMap {
+    constructor(initialLength = 16) {
+        this.table = new Array(initialLength).fill(null)
+        this.size = 0
+    }
+
+    hash(key) {
+        let hashCode = 0
+        const primeNumber = 31
+        for (let i = 0; i < key.length; i++) {
+            hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % this.table.length
+        }
         return hashCode;
     } 
 
     set(key, value) {
-        this.table[this.hash(key)] = value
-        console.log(this.table)
-    }
-
-    get(key) {
-        if (this.table[this.hash(key)]) {
-            return console.log(this.table[this.hash(key)])
+        let index = this.hash(key)
+        if (!this.table[index]) {
+            let element = new LinkedList()
+            element.append({key, value})
+            this.table[index] = element
         } else {
-            return console.log(null)
-        }
-    }
+            let IsItCollision = true
 
-    has(key) {
-        if (this.table[this.hash(key)]) {
-            return console.log(true)
-        } else {
-            return console.log(false)
-        }
-    }
-
-    remove(key) {
-        if ((this.table[this.hash(key)])) {
-            delete this.table[this.hash(key)] 
-            return console.log(true)
-        } else {
-            return console.log(false)
-        }
-    }
-
-    length() {
-        let count = 0
-        for (let key in this.table) {
-            if (this.table[key]) {
-                count += 1
+            let link = this.table[index].head.nextNode
+            while (link != null) {
+                if (link.value.key == key) {
+                    link.value.value = value
+                    IsItCollision = false
+                    break
+                }
             }
-        }
-        return console.log(count)
+            if (IsItCollision) {
+                this.table[index].append({key, value})
+            }
+        } 
+        this.show()
     }
-
-    clear() {
-        for (let key in this.table) {
-            delete this.table[key] 
-        }
-    }
-
-    keys() {
-        let array = []
-        for (let key in this.table) {
-            array.push(key)
-        }
-
-        return console.log(array)
-    }
-
-    values() {
-        let array = []
-        for (let key in this.table) {
-            array.push(this.table[key])
-        }
-
-        return console.log(array)
-    }
-
-    entries() {
-        let array = []
-        for (let key in this.table) {
-            let pair = [key, this.table[key]]
-            array.push(pair)
-        }
-        return console.log(array)
+    
+    show() {
+        return console.log(this.table)
     }
 }
 
@@ -96,22 +205,22 @@ let table = new HashMap()
 table.set('Simon', '89850567307')
 table.set('Igor', '898505673071')
 table.set('Anna', '898505673072')
-table.set('Anna', '898505673073')
 
-table.get('Anna')
-table.get('Sasha')
 
-table.has('Anna')
-table.has('Sasha')
+// table.get('Anna')
+// table.get('Sasha')
 
-table.remove('Anna')
+// table.has('Anna')
+// table.has('Sasha')
 
-table.length()
+// table.remove('Anna')
 
-// table.clear()
+// table.length()
 
-table.keys()
+// // table.clear()
 
-table.values()
+// table.keys()
 
-table.entries()
+// table.values()
+
+// table.entries()
